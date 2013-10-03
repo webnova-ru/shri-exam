19553efebec7fd1079097d631222547bd383aad3
Mikhail Makhotkin <mikhailmakhotkin@gmail.com>
2013-10-03 14:16:28 +0400

    Создана страница просмотра лекции

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,14 +1,17 @@
 $(function(){
     $('[data-validate="formNova"]').formNova();
 
-    $('#js-edit-student').on('click', function(){
+    // перейти на редактирование
+    $('#js-edit-student').on('click', function(e){
         $('#js-view-student').toggleClass('-btn--active');
         $(this).toggleClass('-btn--active');
         $('.student-info-full').hide();
         $('.student-edit').show();
         return false;
     });
-    $('#js-view-student').on('click', function(){
+
+    // перейти на просмотр
+    $('#js-view-student').on('click', function(e){
         $('#js-edit-student').toggleClass('-btn--active');
         $(this).toggleClass('-btn--active');
         $('.student-edit').hide();
