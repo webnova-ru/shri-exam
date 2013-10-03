a0f9aabef1d3b527eb9f7bbe55c4c29e2faa10bf
Mikhail Makhotkin <mikhailmakhotkin@gmail.com>
2013-10-03 12:48:09 +0400

    Добавлен режим переключения между редактирование и просмотром анкеты студента

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,18 @@
 $(function(){
     $('[data-validate="formNova"]').formNova();
+
+    $('#js-edit-student').on('click', function(){
+        $('#js-view-student').toggleClass('-btn--active');
+        $(this).toggleClass('-btn--active');
+        $('.student-info-full').hide();
+        $('.student-edit').show();
+        return false;
+    });
+    $('#js-view-student').on('click', function(){
+        $('#js-edit-student').toggleClass('-btn--active');
+        $(this).toggleClass('-btn--active');
+        $('.student-edit').hide();
+        $('.student-info-full').show();
+        return false;
+    });
 });
