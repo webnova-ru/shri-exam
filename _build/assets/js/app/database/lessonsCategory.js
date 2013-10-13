06dc5f3a7872cf01b0daa50184f42fd738dcc57f
Mikhail Makhotkin <mikhailmakhotkin@gmail.com>
2013-10-13 20:46:30 +0400

    Изменена структура файлов проекта, сконкатенированы файлы для билда

--- _build/assets/js/app/database/lessonsCategory.js
+++
@@ -1,1 +0,0 @@
-define(["can","fixture"],function(){var e=[{id:1,name:"Инструменты разработки"},{id:2,name:"Технология разработки UI"},{id:3,name:"Языки программирования"}];return can.fixture("GET /category",function(){return e}),can.fixture("GET /category/{id}",function(t){return e[t.data.id-1]}),e});
--- _build/assets/js/app/database/teachers.js
+++
@@ -1,1 +0,0 @@
-define(["can","fixture"],function(){var t=[{id:1,name:"Виктор Ашик"},{id:2,name:"Сергей Сергеев"},{id:3,name:"Игорь Новак"},{id:4,name:"Роман Андриади"},{id:5,name:"Евгений Дорошенко"},{id:6,name:"Алексей Бережной"},{id:7,name:"Денис Бугарчев"}];return can.fixture("GET /teachers",function(){return t}),can.fixture("GET /teacher/{id}",function(e){return t[e.data.id-1]}),t});
